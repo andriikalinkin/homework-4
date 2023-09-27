4a18f7d40a293c475e669d3bfe0c3272bbb1d4c7
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-09-27 13:11:13 +0300

    Fix settings.py INSTALLED_APPS Makemigrations Migrate

--- homework_4/homework_4/settings.py
+++ homework_4/homework_4/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'student.apps.StudentConfig'
+    'student.apps.StudentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
